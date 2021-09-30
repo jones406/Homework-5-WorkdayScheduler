33b703273b641291f17088334cfe3d8bbd641d50
brooke <brookejones406@gmail.com>
2021-09-30 17:04:09 -0500

    posttutorsesh

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,50 @@ let descriptionEl = $(".description");
 let hourEl = $(".hour");
 
 // event listner on save button that console.logs the value of the textarea
-saveBtnEl.click(function() {
-  console.log(descriptionEl);
+$(document).ready(function() { // makes js wait until DOM is loaded
+
 })
+
+saveBtnEl.click(function() { // we attach this to each element in the .each() method
+  console.log(descriptionEl.val());
+  localStorage.setItem("task9", descriptionEl.val())
+})
+
+//there are multiple save buttons and multiple descriptionEls
+
+$(".row").each(function(index, element) {
+  console.log("index", index)
+  // console.log("element", element)
+  console.log($(element).children("button"))
+  // look up "this" or use the element
+  // select each elements button
+  // attach event listener
+})
+
+// jQuery .each() method
+
+// if (localStorage.getItem("task")) {
+//   console.log("true")
+// } else {
+//   console.log("false")
+// }
+
+// let tasks = localStorage.getItem("tasks") ||
+// [
+//   {nine: ""},
+//   {ten: ""},
+//   {eleven: ""},
+//   {twelve: ""},
+//   {one: ""},
+//   {two: ""},
+//   {three: ""},
+//   {four: ""},
+//   {five: ""}
+// ]
+
+
+// localStorage.setItem("name", "Brooke")
+
+// let name = localStorage.getItem("name")
+
+// console.log(name)
