d1cba7ea4c51620f605b9155cb749420de6c9b50
brooke <brookejones406@gmail.com>
2021-09-30 16:20:45 -0500

    jsfile

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+// Global Variables
+let saveBtnEl = $(".saveBtn");
+let descriptionEl = $(".description");
+let hourEl = $(".hour");
+
+// event listner on save button that console.logs the value of the textarea
+saveBtnEl.click(function() {
+  console.log(descriptionEl);
+})
