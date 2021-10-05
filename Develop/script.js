25768e5943c42e79f1fb208cb5d57525c0c83c6a
brooke <brookejones406@gmail.com>
2021-10-04 23:01:08 -0500

    Timer updating properly, local storageand local storage working properly

--- Develop/script.js
+++ Develop/script.js
@@ -1,71 +1,43 @@
-// Global Variables
-let saveBtnEl = $(".saveBtn"); //selects all instances of class = "saveBtn"
-let descriptionEl = $(".description");//selects all instances of class = "desription"
-let hourEl = $(".hour");//selects all instances of class = "hour"
+// Date/time update every min. Use setInterval() method.
+setInterval(currentTimeHey, 60000);
 
-
-//grab the current date and display it in the header within the p tag with the id of currentDay
-let dateTime = moment().format('LLLL');   
-console.log(dateTime);
-displaydateTime = $("#currentDay").text(dateTime);
-
-
-$(document).ready(function() { // makes js wait until DOM is loaded
-
-let timeComparison = 
+//grab date/time from moment API 
+function currentTimeHey() {
+  let dateTime = moment().format('LLLL');
+  $("#currentTime").text(dateTime); //display in header
+  //console.log(dateTime);  used to check
 }
 
-})
+currentTimeHey(); //call fn
 
-
-//save text from time block in local storage when save button clicked
-saveBtnEl.click(function() { // we attach this to each element in the .each() method
-  console.log(descriptionEl.val());
-  localStorage.setItem("task", descriptionEl.val())
-})
-
-
-//problem: there are multiple save buttons and multiple descriptionEls
-
-$(".row").each(function(index, element) {
-  console.log("index", index)
-  // console.log("element", element)
+//will need to grab and manipulate the children of the divs w/ class of "row"
+$(".row").each(function (index, element) { //grab each element w/ this class
+  console.log("index", index) // creates an index number for as many divs that have class="row". Spits out 0-8
   console.log($(element).children("button"))
-  // look up "this" or use the element
-  // select each elements button
-  // attach event listener
+  console.log($(element).children("input"))
+  
+  timeChecker(element); //run the timeChecker fn
+
+  // setting inputs with saved tasks
+  let task = localStorage.getItem("task-" + index);
+  console.log(task);
+  $(element).children("input").val(task); //grab input element's value (whatever user typed in) and put it in local storage
+
+  // attaching click listener to each button
+  $(element).children("button").click(function (event) {
+    event.preventDefault();
+    console.log($(element).children("input").val())
+    localStorage.setItem("task-" + index, $(element).children("input").val());
+  })
 })
 
-
-
-// use jquery to grab all elements on page by class and loop thru them... and then affect their css that way, in that loop. Use data attributes wisely...put a lot of info in your HTML so you can use jquery -->
-
-// jQuery .each() method
- 
 //styling example: grabbedElVariable.style.color = "color"
+function timeChecker(node) {
+  if ($(node).attr("data-hour") < moment().format('HH')) {
 
-// if (localStorage.getItem("task")) {
-//   console.log("true")
-// } else {
-//   console.log("false")
-// }
-
-// let tasks = localStorage.getItem("tasks") ||
-// [
-//   {nine: ""},
-//   {ten: ""},
-//   {eleven: ""},
-//   {twelve: ""},
-//   {thirteen: ""},
-//   {fourteen: ""},
-//   {fifteen: ""},
-//   {sixteen: ""},
-//   {seventeen: ""}
-// ]
+  }
 
 
-// localStorage.setItem("name", "Brooke")
-
-// let name = localStorage.getItem("name")
-
-// console.log(name)
+    console.log($(node).attr("data-hour"))
+    console.log(moment().format('HH'))
+  }
